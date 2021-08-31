f0b4985dc5499daed07f325e459b91348817c867
Sulaimanma <379043678@gmail.com>
2021-09-01 01:22:41 +1000

    Update the cheese details of the click one

--- src/client/Cart/CheeseDetailDialog/CheeseDetailDialog.tsx
+++ src/client/Cart/CheeseDetailDialog/CheeseDetailDialog.tsx
@@ -1,63 +1,148 @@
-import React from "react"
-import Button from "@material-ui/core/Button"
-import Dialog from "@material-ui/core/Dialog"
-import DialogActions from "@material-ui/core/DialogActions"
-import DialogContent from "@material-ui/core/DialogContent"
-import DialogContentText from "@material-ui/core/DialogContentText"
-import DialogTitle from "@material-ui/core/DialogTitle"
-import Slide from "@material-ui/core/Slide"
-import { TransitionProps } from "@material-ui/core/transitions"
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Slide from '@material-ui/core/Slide';
+import { TransitionProps } from '@material-ui/core/transitions';
+import {
+  makeStyles,
+  createStyles,
+  Theme,
+} from '@material-ui/core/styles';
+import { Grid } from '@material-ui/core';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import ButtonBase from '@material-ui/core/ButtonBase';
+
+//Glass Magnifier to see the details of the image
+import { GlassMagnifier } from 'react-image-magnifiers';
 //Styles
-import { Wrapper } from "./CheeseDetailDialog.styles"
+import { Wrapper } from './CheeseDetailDialog.styles';
 //Types
-import { CartItemType } from "../../App"
+import { CartItemType } from '../../App';
 
 type Props = {
-  open: boolean
-  setOpen: (openStatus: boolean) => void
-  cheeseItem: CartItemType
-}
+  open: boolean;
+  setOpen: (openStatus: boolean) => void;
+  cheeseItem: CartItemType;
+};
 
+//define the style
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      margin: 'auto',
+      minWidth: 600,
+      // maxWidth: 1000,
+    },
+    image: {
+      maxWidth: 512,
+      maxHeight: 512,
+    },
+    img: {
+      margin: 'auto',
+      display: 'block',
+      maxWidth: '100%',
+      maxHeight: '100%',
+    },
+  })
+);
+//Define the dialog transition
 const Transition = React.forwardRef(function Transition(
-  props: TransitionProps & { children?: React.ReactElement<any, any> },
+  props: TransitionProps & {
+    children?: React.ReactElement<any, any>;
+  },
   ref: React.Ref<unknown>
 ) {
-  return <Slide direction="up" ref={ref} {...props} />
-})
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 
-const CheeseDetailDialog: React.FC<Props> = ({ open, setOpen, cheeseItem }) => {
+const CheeseDetailDialog: React.FC<Props> = ({
+  open,
+  setOpen,
+  cheeseItem,
+}) => {
   const handleClose = () => {
-    setOpen(false)
-  }
+    setOpen(false);
+  };
+
+  const classes = useStyles();
 
   return (
-    <Wrapper>
-
+    <div>
+      <Wrapper className={classes.root}>
         <Dialog
           open={open}
           TransitionComponent={Transition}
           keepMounted
           onClose={handleClose}
-          aria-labelledby="alert-dialog-slide-title"
-          aria-describedby="alert-dialog-slide-description"
+          maxWidth="lg"
         >
-          <DialogTitle id="alert-dialog-slide-title">
-            {cheeseItem.title}
-          </DialogTitle>
-          <DialogContent>
-            <DialogContentText id="alert-dialog-slide-description">
-             {cheeseItem.description}
-            </DialogContentText>
-          </DialogContent>
+          <Paper className={classes.paper}>
+            <Grid container spacing={2}>
+              <Grid item>
+                <ButtonBase className={classes.image}>
+                  <GlassMagnifier
+                    className={classes.img}
+                    imageSrc={cheeseItem.image}
+                    imageAlt={`${cheeseItem.image} detail image`}
+                  />
+                </ButtonBase>
+              </Grid>
+              <Grid item xs={12} sm container>
+                <Grid
+                  item
+                  xs
+                  container
+                  direction="column"
+                  spacing={2}
+                >
+                  <Grid item xs>
+                    <Typography
+                      gutterBottom
+                      variant="subtitle1"
+                    >
+                      {cheeseItem.title}
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      gutterBottom
+                    >
+                      Category: {cheeseItem.category}
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      color="textSecondary"
+                    >
+                      Description:
+                      {cheeseItem.description}
+                    </Typography>
+                  </Grid>
+                </Grid>
+                <Grid item>
+                  <Typography variant="subtitle1">
+                    ${cheeseItem.price}
+                  </Typography>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Paper>
           <DialogActions>
             <Button onClick={handleClose} color="primary">
               Close
             </Button>
           </DialogActions>
         </Dialog>
-   
-    </Wrapper>
-  )
-}
+      </Wrapper>
+    </div>
+  );
+};
 
-export default CheeseDetailDialog
+export default CheeseDetailDialog;
