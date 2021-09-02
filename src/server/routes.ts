e2ac6e4f50dea62f9b290908ce37b0fe5b84860a
Sulaimanma <379043678@gmail.com>
2021-09-02 23:52:25 +1000

    Connect the local purchaselist to MogonDB database

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -1,11 +1,15 @@
 import * as express from 'express';
 const cheeses = require('./data/cheeses.json');
+import mongoose = require('mongoose');
+import Purchase from './data/models/purchase';
 
 //Purchase list json
 
-const purchaseList: any[] = [];
+// const purchaseList: any[] = [];
 
 const router = express.Router();
+
+// const Purchase = require('./data/models/purchase');
 
 router.get('/api/cheeses', (req, res, next) => {
   res.json(cheeses);
@@ -14,14 +18,34 @@ router.get('/api/cheeses', (req, res, next) => {
 router.post('/api/purchase', (req, res, next) => {
   // Authenticate the user
   const { paymenttoken } = req.headers;
-
+  console.log('paymentToken', paymenttoken);
   if (paymenttoken && paymenttoken === 'sulaiman') {
     const purchaseRecord: any[] = req.body;
 
-    //Update the purchase list
-    purchaseRecord?.map((record) => purchaseList.push(record));
-
-    res.status(201).send('Purchase record has been created!');
+    //Loop each record to be posted to the database
+    purchaseRecord?.map(async (record) => {
+      //Read the request data into schema to validate
+      const purchase = new Purchase({
+        id: record.id,
+        amount: record.amount,
+        description: record.description,
+        image: record.image,
+        price: record.price,
+        purchaseTime: record.purchaseTime,
+        title: record.title,
+      });
+      return (
+        purchase
+          //Upload the data to database
+          .save()
+          .then((result) => {
+            console.log('post', result);
+            return res.status(201).send('Purchase record has been created!');
+          })
+          .catch((err: string) => console.log(err))
+      );
+    });
+    console.log('req.body', req.body);
   } else {
     res.status(403).send('Forbidden');
   }
@@ -31,7 +55,13 @@ router.post('/api/purchase', (req, res, next) => {
 
 //Access purchase history
 router.get('/api/purchase', (req, res, next) => {
-  res.status(200).send(purchaseList);
+  // Get data from database
+  Purchase.find()
+    .then((list) => {
+      console.log(list);
+      res.status(200).json(list);
+    })
+    .catch((err: string) => console.log(err));
 });
 
 export default router;
